af836a6e3465102555698409ad6510c61103921c
André Calazans <andcalazans@hotmail.com>
2021-11-26 15:17:50 -0300

    Display transactions in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -14,14 +15,47 @@ class ExpensesApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   // const MyHomePage({ Key? key }) : super(key: key);
 
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 739.20,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            // width: double.infinity,
+            child: const Card(
+              color: Colors.cyan,
+              child: Text('Gráfico'),
+              elevation: 2,
+            ),
+          ),
+          Column(
+            children: _transactions.map((e) {
+              return Card(
+                child: Text(e.title.toString()),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
